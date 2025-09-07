987790573439b9819b0cd3662c482fd586a88988
Fábio Valentim <maitabom@gmail.com>
2025-09-06 22:18:45 -0300

    Adiciona o formulário de locais com campo de título e componente de imagem

--- lib/pages/place_form.dart
+++ lib/pages/place_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:locations/components/image_input.dart';
 
 class PlaceFormPage extends StatefulWidget {
   const PlaceFormPage({super.key});
@@ -8,11 +9,45 @@ class PlaceFormPage extends StatefulWidget {
 }
 
 class _PlaceFormPageState extends State<PlaceFormPage> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Novo Local')),
-      body: Center(child: Text('Formulário')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Titulo'),
+                    controller: _titleController,
+                  ),
+                  SizedBox(height: 10),
+                  ImageInput(),
+                ],
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            label: Text('Adicionar'),
+            icon: Icon(Icons.add_location_alt),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
