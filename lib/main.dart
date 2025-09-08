da14df6b92ac974980e49828b0f0883ed89a3a3d
Fábio Valentim <maitabom@gmail.com>
2025-09-07 23:51:08 -0300

    altera a função main para ser assíncrona e carregar variáveis de ambiente do arquivo .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:locations/pages/place_form.dart';
 import 'package:locations/pages/places_list.dart';
 import 'package:locations/providers/great_places.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
   runApp(const MyApp());
 }
 
